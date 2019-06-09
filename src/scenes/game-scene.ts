5880e2b0d6f35941fc154f78650dc512a84a5264
Shawn Taylor <shawn@mapsandapps.net>
2019-06-09 15:41:46 -0400

    Collision with walls

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -2,9 +2,11 @@ import { Player } from '../objects/player'
 
 export class GameScene extends Phaser.Scene {
   private backgroundLayer: Phaser.Tilemaps.StaticTilemapLayer
+  private debug: boolean
   private layer: Phaser.Tilemaps.StaticTilemapLayer
   private map: Phaser.Tilemaps.Tilemap
   private tileset: Phaser.Tilemaps.Tileset
+  private walls: Phaser.Physics.Arcade.StaticGroup
 
   private player: Player
 
@@ -16,7 +18,37 @@ export class GameScene extends Phaser.Scene {
 
   init(): void {}
 
+  private createWall(x, y, width, height): void {
+    this.walls.add(new Phaser.GameObjects.Rectangle(this, x, y, width, height, 0xf00000, 1).setOrigin(0, 0), this.debug)
+  }
+
+  private addWallToTile(tile): void {
+    const WALL_WIDTH = 1
+    if (tile.properties.preventDown) {
+      this.createWall(tile.pixelX, tile.pixelY + tile.height - WALL_WIDTH, tile.width, WALL_WIDTH)
+    }
+    if (tile.properties.preventLeft) {
+      this.createWall(tile.pixelX, tile.pixelY, WALL_WIDTH, tile.height)
+    }
+    if (tile.properties.preventRight) {
+      this.createWall(tile.pixelX + tile.width - WALL_WIDTH, tile.pixelY, WALL_WIDTH, tile.height)
+    }
+    if (tile.properties.preventUp) {
+      this.createWall(tile.pixelX, tile.pixelY, tile.width, WALL_WIDTH)
+    }
+  }
+
+  private addWalls(): void {
+    this.map.layer.data.forEach((row: any) => {
+      row.forEach(tile => {
+        this.addWallToTile(tile)
+      })
+    })
+  }
+
   create(): void {
+    this.debug = true
+
     this.map = this.make.tilemap({ key: 'maze' })
 
     this.tileset = this.map.addTilesetImage('maze', 'tiles', 64, 64, 0, 0)
@@ -24,6 +56,11 @@ export class GameScene extends Phaser.Scene {
     this.layer = this.map.createStaticLayer('Map', this.tileset, 0, 0)
 
     this.convertObjects()
+
+    this.walls = this.physics.add.staticGroup()
+    this.addWalls()
+
+    this.physics.add.collider(this.player, this.walls)
 
     this.cameras.main.startFollow(this.player)
   }
